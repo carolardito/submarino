6b30d0460f1e584fea4e6b31a178bfd0e2ff3331
cleitonG <cleiton-g-s@hotmail.com>
2014-09-08 20:38:33 -0300

    banco_submarino

---
+++ banco_submarino.sql
@@ -0,0 +1,39 @@
+CREATE TABLE produto (
+    cod_produto INT not null primary key
+        GENERATED ALWAYS AS IDENTITY
+        (START WITH 1, INCREMENT BY 1),
+    nome_produto VARCHAR(50) not null,
+    preco DOUBLE not null,
+    descricao VARCHAR(10)
+);
+
+CREATE TABLE item (
+    cod_item INT not null primary key
+        GENERATED ALWAYS AS IDENTITY
+        (START WITH 1, INCREMENT BY 1),
+    cod_produto INT not null ,
+    quantidade INT not null,
+    FOREIGN KEY (cod_produto) REFERENCES submarino.produto(cod_produto)
+    
+);
+
+CREATE TABLE ceps (
+    cep INT not null primary key,
+    preco DOUBLE not null    
+);
+
+    
+CREATE TABLE lista_item (
+    cod_lista_item INT not null,
+    cod_item INT not null,
+    FOREIGN KEY (cod_item) REFERENCES submarino.item(cod_item)
+);
+
+CREATE TABLE carrinho (
+     cod_carrinho INT not null primary key
+        GENERATED ALWAYS AS IDENTITY
+        (START WITH 1, INCREMENT BY 1),
+     cod_lista_item INT not null,
+     cep INT,
+     FOREIGN KEY (cep) REFERENCES submarino.ceps(cep)
+);
