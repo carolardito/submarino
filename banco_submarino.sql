c985f6e577af21f9d48900382ba22948b6466443
cleitonG <cleiton-g-s@hotmail.com>
2014-09-23 20:27:51 -0300

    Update banco_submarino.sql

--- banco_submarino.sql
+++ banco_submarino.sql
@@ -31,3 +31,10 @@ CREATE TABLE item (
     
 );
 
+INSERT INTO produto VALUES("televisão",2000,"smartv de 50 polegadas");
+INSERT INTO produto VALUES("radio",100,"radio de pilha");
+INSERT INTO produto VALUES("sofa",3000,"sofa super confortavel de super molas");
+
+INSERT INTO ceps VALUES("04836-250",20);
+INSERT INTO ceps VALUES("02038-200",40);
+INSERT INTO ceps VALUES("01050-100",30);
